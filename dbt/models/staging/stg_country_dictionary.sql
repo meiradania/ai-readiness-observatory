56ca898d310afe389e5363281f9f46e18e29785b
Dânia Meira <meiradania@gmail.com>
2026-08-04 13:37:42 +0000

    documented models

--- dbt/models/staging/stg_country_dictionary.sql
+++ dbt/models/staging/stg_country_dictionary.sql
@@ -8,15 +8,9 @@ with source as (
 renamed as (
 
     select
-<<<<<<< HEAD
         lower(country_name) as country_name,
         upper(iso2) as country_code_iso2,
         upper(iso3) as country_code_iso3
-=======
-        trim(lower(country_name)) as country_name,
-        trim(upper(iso2)) as country_code_iso2,
-        trim(upper(iso3)) as country_code_iso3
->>>>>>> 3160005 (schema.yml files for staging tables and mart tables.)
 
     from source
 
